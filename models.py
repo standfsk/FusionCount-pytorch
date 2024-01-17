5def7e728f4546aa6e225a08c1986a9949988e0a
standfsk <60250322+standfsk@users.noreply.github.com>
2024-01-17 10:15:43 +0900

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from torch import Tensor, nn
 import torch.nn.functional as F
 from helpers import _initialize_weights, ConvNormActivation, FeatureFuser, ChannelReducer
 from vgg import VGG
-import torch.utils.model_zoo as model_zoo
 
 class FusionCount(nn.Module):
     """
@@ -71,55 +70,6 @@ class FusionCount(nn.Module):
         mu_normed = mu / (mu_sum + 1e-6)
         return mu, mu_normed
 
-model_urls = {
-    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
-    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
-}
-class VGG(nn.Module):
-    def __init__(self, features):
-        super(VGG, self).__init__()
-        self.features = features
-        self.reg_layer = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-        )
-        self.density_layer = nn.Sequential(nn.Conv2d(128, 1, 1), nn.ReLU())
-
-    def forward(self, x):
-        x = self.features(x)
-        x = F.upsample_bilinear(x, scale_factor=2)
-        x = self.reg_layer(x)
-        mu = self.density_layer(x)
-        B, C, H, W = mu.size()
-        mu_sum = mu.view([B, -1]).sum(1).unsqueeze(1).unsqueeze(2).unsqueeze(3)
-        mu_normed = mu / (mu_sum + 1e-6)
-        return mu, mu_normed
-
-def make_layers(cfg, batch_norm=False):
-    layers = []
-    in_channels = 3
-    for v in cfg:
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-            else:
-                layers += [conv2d, nn.ReLU(inplace=True)]
-            in_channels = v
-    return nn.Sequential(*layers)
-
-cfg = {
-    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512]
-}
-
-def vgg19(args):
-    """VGG 19-layer model (configuration "E")
-        model pre-trained on ImageNet
-    """
     model = VGG(make_layers(cfg['E']))
     model.load_state_dict(model_zoo.load_url(model_urls[args.backbone]), strict=False)
     return model
