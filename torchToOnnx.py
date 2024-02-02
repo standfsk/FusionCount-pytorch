9378f512081b42a06e5ae147922b7d690cb72081
standfsk <60250322+standfsk@users.noreply.github.com>
2024-02-02 15:10:34 +0900

    Update torchToOnnx.py

--- torchToOnnx.py
+++ torchToOnnx.py
@@ -11,10 +11,10 @@ from onnxsim import simplify
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Test ')
-    parser.add_argument('--device', default='3', help='assign device')
+    parser.add_argument('--device', default='0', help='assign device')
     parser.add_argument('--crop-size', type=int, default=512,
                         help='the crop size of the train image')
-    parser.add_argument('--weight_path', type=str, default='ckpt/sha_shb_qnrf/best_model.pth',
+    parser.add_argument('--weight_path', type=str, default='ckpt/ucfcc/best_model.pth',
                         help='saved model path')
     parser.add_argument('--backbone', default='vgg16')
     parser.add_argument('--save_path', default='')
@@ -24,14 +24,14 @@ def get_args_parser():
 
 
 def main(args):
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.device  # set vis gpu
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device
     device = torch.device('cuda')
 
     model_path = args.weight_path
     onnx_name = args.save_path
     dataset_name = 'UCF_CC_50'
 
-    dataset = crowd.Crowd_(os.path.join('dataset', dataset_name, 'test'), args.crop_size, 8, method='test')
+    dataset = crowd.Crowd_no(os.path.join('dataset', dataset_name, 'test'), args.crop_size, 8, method='test')
     dataloader = torch.utils.data.DataLoader(dataset, 1, shuffle=False, num_workers=1, pin_memory=True)
     inputs, name = next(iter(dataloader))
     inputs = inputs.to(device)
@@ -46,9 +46,10 @@ def main(args):
         torch.onnx.export(model, inputs, onnx_name, opset_version=12)
 
     onnx_model = onnx.load(onnx_name)
-    onnx_sim, check = simplify(onnx_model)
+    onnx_sim, check = simplify(onnx_model, skip_shape_inference=True)
     assert check, "Simplified ONNX model could not be validated"
     onnx.save(onnx_sim, onnx_name)
+    print("onnx saved!!!")
 
 if __name__ == "__main__":
     args = get_args_parser()
